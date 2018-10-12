1a1366bd058ad5f9005426ff8a9a2cb0189a74c7
misterphi <misterphi@misterphimacbook>
2018-10-12 11:54:01 +0200

    debug plantage Diagnostics

--- sources/var/www/sambaedu/tests/test_web.php
+++ sources/var/www/sambaedu/tests/test_web.php
@@ -5,23 +5,13 @@
    * Test une requete sur le web wawadeb
    * @Projet LCS / SambaEdu
    * @Auteurs Equipe Sambaedu
-   * @Licence Distribue sous la licence GPLlancement des scripts bash par la technologie asynchrone Ajax.
-
-    * @Licence Distribue sous la licence GPL
-
-
-   */
-
-   /**
-
+   * @Licence Distribue sous la licence GPL
    * @Repertoire: /tests/
    * file: test_web.php
    */
 
-
-
 require_once('entete_ajax.inc.php');
-   $http=exec("cd /tmp; wget -q --tries=1 --timeout=2 http://sambaedu.org && echo \$? | rm -f /tmp/index.html.1*",$out,$retour);
+   $http=exec("cd /tmp; wget -q --tries=1 --timeout=2 https://ac-caen.fr && echo \$? | rm -f /tmp/index.html*",$out,$retour);
 
    if ($retour=="0") {
    	$ok="1";
