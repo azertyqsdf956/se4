29b41956822b833172af9352419faa8e3384052d
Denis Bonnenfant <denis.bonnenfant@diderot.org>
2018-10-17 18:10:04 +0200

    paquet de mise en place du client linux ad fonctionnel

--- sources/usr/share/sambaedu/includes/utils.inc.sh
+++ sources/usr/share/sambaedu/includes/utils.inc.sh
@@ -22,17 +22,20 @@ my_network() {
 
 IFS=" "
 read my_gateway my_interface<<<$(ip -o -f inet route show default 0.0.0.0/0 | cut -d ' ' -f3,5)
-read my_address my_cdr my_broadcast my_mode<<<$(ip -o -f inet addr show dev "$my_interface" | awk '{sub("/", " ", $4); print $4, $6, $9}')
+read my_address my_cdr my_broadcast<<<$(ip -o -f inet addr show dev "$my_interface" | awk '{sub("/", " ", $4); print $4, $6}')
 my_mask=$(cdr2mask $my_cdr)
 IFS=
 my_network=$(ip -o -f inet route show dev $my_interface  src $my_address | cut -d/ -f1)
 my_hostname=$(hostname -s 2>/dev/null)
-my_domain=$(hostname -d 2>/dev/null) || true
+#my_domain=$(hostname -d 2>/dev/null) || true
 my_fqdn=$(hostname -f 2>/dev/null) || true
 # attention ne donne pas le dns externe si ad est configuré
 my_dnsserver=$(grep -m 1 "^nameserver" /etc/resolv.conf | cut -d" " -f2) || true
+my_domain=$(grep -e "^domain\s*.*$" /etc/resolv.conf | cut -d" " -f2) || true
 my_proxy="$http_proxy"
 if [ -e "etc/sambaedu/sambaedu.conf.d/dhcp.conf" ]; then
     my_vlan=$(grep $my_network /etc/sambaedu/sambaedu.conf.d/dhcp.conf | cut -d= -f1 | sed "s/^.*_//")
 fi
+filtre="^\s*iface\s*${my_interface}\s*inet.*$"
+my_mode=$(grep -e "$filtre" /etc/network/interfaces | cut -d" " -f4)
 }
