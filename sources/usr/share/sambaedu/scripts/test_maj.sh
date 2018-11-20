1d2ecbb12a20059f701aa71009866b8090a0d3d7
mollef <franck.molle@ac-rouen.fr>
2018-11-20 02:06:24 +0100

    Remise en route page info sur les maj SE disponibles

--- sources/usr/share/sambaedu/scripts/test_maj.sh
+++ sources/usr/share/sambaedu/scripts/test_maj.sh
@@ -10,23 +10,22 @@ then
 	echo "Usage : aucune option"
 	exit
 fi
-tmpdir="$(mktemp)"
+tmpfile="$(mktemp)"
+maj="no"
 dpkg -l|grep "sambaedu" |cut -d ' ' -f3|while read package
 do
 # # LC_ALL=C apt-get -s install $package|grep newest >/dev/null|| echo $package
-
 mod_install=$(apt-cache policy $package | grep "Install" | cut -d" " -f4)
 mod_candidat=$(apt-cache policy $package | grep "Candidat" | cut -d" " -f4)
     if [ "$mod_install" != "$mod_candidat" ]; then
-#         echo "$package pas glop"
-        MAJ=0
-#     else
-#             echo "$package glop"
+        echo "$package pas glop"
+        echo "yes" > "$tmpfile"
+        echo $maj
+        break
     fi
 done
 
-if [ "$MAJ" = "0" ]
-then
+if [ -n $(grep "yes" "$tmpfile") ]; then
     echo "0"
     exit 0
 else
